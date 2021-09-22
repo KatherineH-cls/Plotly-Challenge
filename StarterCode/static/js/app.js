8c3d06b67bb739a136d634797c166d90156cdf07
KatherineH-cls <katherine.howison@bigpond.com>
2021-09-22 09:41:32 +0800

    added ids to drop down box

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,54 +5,71 @@ d3.json("samples.json").then((data) => {
     var id = "940";
 
     var samples = data.samples;
+    var names = data.names;
+    console.log(names);
+
+    //add list of test subjects to dropdown box
+    var options = d3.select("#selDataset");
+    names.forEach((name, index) => {
+        var row = options.append("option");
+        row.property("value", index);
+        row.text(name);
+    });
+      
+
+    // <option value="dataset1">US</option>
+
+    // function to draw the bar chart
+    function barChart(id) {
+        // Create empty arrays to store the bar chart data
+        var bact = [];
+
+        // Iterate through each sample
+        samples.forEach((sample) => {
+
+            if (sample.id === id) {
+                console.log(sample);
+                var labels = sample.otu_labels;
+                var values = sample.sample_values;
+                var otu_id = sample.otu_ids;
+
+                console.log(otu_id);
+
+                // add to array
+                for (i = 0; i < labels.length; i++) {
+                    bact.push({ b_label: labels[i], b_value: values[i], b_id: otu_id[i] });
+                };
+                console.log(bact);
+                bactSorted = bact.sort((a, b) => b.b_value - a.b_value).slice(0, 10).reverse();
+                console.log(bactSorted);
 
 
-    // Create empty arrays to store the bar chart data
-    var bact = [];
-
-    // Iterate through each sample
-    samples.forEach((sample) => {
-
-        if (sample.id === id) {
-            console.log(sample);
-            var labels = sample.otu_labels;
-            var values = sample.sample_values;
-            var otu_id = sample.otu_ids;
-
-            console.log(otu_id);
-
-            // add to array
-            for (i = 0; i < labels.length; i++) {
-                bact.push({ b_label: labels[i], b_value: values[i], b_id: otu_id[i] });
-            };
-            console.log(bact);
-            bactSorted = bact.sort((a, b) => b.b_value - a.b_value).slice(0, 10).reverse();
-            console.log(bactSorted);
-
-            
-            // horizontal bar chart
-            var trace_bar = {
-                type: 'bar',
-                x: bactSorted.map(bactSorted => bactSorted.b_value),
-                y: bactSorted.map(bactSorted => `OTU ${bactSorted.b_id}`),
-                orientation: 'h',
-                text: bactSorted.map(bactSorted => bactSorted.b_label),
-                marker: {color: 'rgb(142,124,195)'}
+                // horizontal bar chart
+                var trace_bar = {
+                    type: 'bar',
+                    x: bactSorted.map(bactSorted => bactSorted.b_value),
+                    y: bactSorted.map(bactSorted => `OTU ${bactSorted.b_id}`),
+                    orientation: 'h',
+                    text: bactSorted.map(bactSorted => bactSorted.b_label),
+                    marker: { color: 'rgb(142,124,195)' }
                 };
 
-            var data = [trace_bar];  
-            
-            var layout = {
-                title: "Common Bacteria",
-                xaxis: { title: "Frequency" },
-                yaxis: { title: "Bacteria Grouping" }
-              };
+                var data = [trace_bar];
 
-              Plotly.newPlot("bar", data, layout);
+                var layout = {
+                    title: "Common Bacteria",
+                    xaxis: { title: "Frequency" },
+                    yaxis: { title: "Bacteria Grouping" }
+                };
 
-        }
-        
-    });
+                Plotly.newPlot("bar", data, layout);
+
+            }
+
+        });
+    }
+
+    barChart(id);
 
 
 
